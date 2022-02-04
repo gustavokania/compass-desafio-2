3162b3a004031e915413d6efbf7d09474d8d6468
Gustavo Kania <gustavokania17@hotmail.com>
2022-02-04 17:22:52 -0300

    Ajustando e adicionando partes do JS da pagina checklist D5

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const section = document.getElementById("section-human");
-const items = section.getElementsByClassName("divisao-principal");
+function checkboxMarcado(sectionName, markerName, lineDescription) {
+  const section = document.getElementById(sectionName);
+  const items = section.getElementsByClassName("divisao-principal");
 
-for (const item of items) {
-  const checkbox = item.getElementsByClassName("marcador1")[0];
-  const line = item.getElementsByClassName("descricao-linha")[0];
+  for (const item of items) {
+    const checkbox = item.getElementsByClassName(markerName)[0];
+    const line = item.getElementsByClassName(lineDescription)[0];
 
-  checkbox.addEventListener('change', evento => {
-    if (evento.target.checked) {
-      line.classList.add("marcador-ok");
-    } else {
-      line.classList.remove("marcador-ok");
-    }
+    checkbox.addEventListener('change', evento => {
+      if (evento.target.checked) {
+        line.classList.add("marcador-ok");
+      } else {
+        line.classList.remove("marcador-ok");
+      }
     });
+  }
 }
+
+checkboxMarcado("section-human", "marcador1", "descricao-linha");
+checkboxMarcado("section-monster", "marcador2", "descricao-linha1");
